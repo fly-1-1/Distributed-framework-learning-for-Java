b8bfbbcd88c499a81333423e6a0a3e3cd14b7478
fly-1-1 <2673024411@qq.com>
2024-03-20 02:25:09 +0800

    sentinel fallback

--- 04springcloud/hmall/hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ 04springcloud/hmall/hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,5 +1,6 @@
 package com.hmall.api.config;
 
+import com.hmall.api.client.fallback.ItemClientFallbackFactory;
 import com.hmall.common.utils.UserContext;
 import feign.Logger;
 import feign.RequestInterceptor;
@@ -26,4 +27,8 @@ public class DefaultFeignConfig {
         };
     }
 
+    @Bean
+    public ItemClientFallbackFactory itemClientFallbackFactory(){
+        return new ItemClientFallbackFactory();
+    }
 }
